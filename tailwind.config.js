0f26bc3ff5e6392f19e121e183c5816efd554c64
Rácz Anna <raczanna@Racz-MacBook-Air.local>
2026-02-02 22:27:28 +0100

    Downgrade to Tailwind v3 for stability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -93,4 +92,3 @@ const config: Config = {
     },
     plugins: [],
 };
-export default config;
